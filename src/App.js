35dd5b524e4524d3687285184e2deb9033879923
Camsz27 <camiloagr2707@gmail.com>
2021-09-20 21:02:07 -0500

    Fixed error of best score increasing by one when player losses while setting his best score

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [lost, setLost] = useState(false);
+
   const increaseScore = () => {
     setScore(score + 1);
-    if (score === 9) {
-      lostGame();
-    }
-    if (score >= bestScore) {
-      setBestScore(score + 1);
-    }
   };
+
   const lostGame = () => {
     setLost(!lost);
   };
 
   useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+    if (score === 10) {
+      lostGame();
+    }
+  }, [score]);
+
+  useEffect(() => {
     setScore(0);
     setLost(false);
+    setBestScore(bestScore);
   }, [lost]);
 
   return (
